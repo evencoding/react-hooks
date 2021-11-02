d79e8846285f5265ea5f8880829ecd19cdd7bd1a
evencoding <88191233+evencoding@users.noreply.github.com>
2021-11-02 13:31:23 +0900

    frist commit

---
+++ useState/index.js
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  const [item, setItem] = useState(1);
+  const incrementItem = () => setItem(item + 1);
+  const decrementItem = () => setItem(item - 1);
+  return (
+    <div className="App">
+      <h1>Hello {item}</h1>
+      <h2>Start editing to see some magic happen!</h2>
+      <button onClick={incrementItem}>incrementItem</button>
+      <button onClick={decrementItem}>decrementItem</button>
+    </div>
+  );
+};
+// class App extends React.Component {
+//   state = {
+//     item: 1,
+//   };
+//   incrementItem = () => {
+//     this.setState((state) => {
+//       return {
+//         item: state.item + 1,
+//       };
+//     });
+//   };
+//   decrementItem = () => {
+//     this.setState((current) => ({ item: current.item - 1 }));
+//   };
+//   render() {
+//     const { item } = this.state;
+//     return (
+//       <div className="App">
+//         <h1>Hello {item}</h1>
+//         <h2>Start editing to see some magic happen!</h2>
+//         <button onClick={this.incrementItem}>incrementItem</button>
+//         <button onClick={this.decrementItem}>decrementItem</button>
+//       </div>
+//     );
+//   }
+// }
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
